fcae6b8d4f19abdda135b7835f2ab050a55c4091
İsmet Ataklı <ismetatakli@gmail.com>
2022-05-17 17:57:53 +0300

    startup.cs changed

--- Blog.Mvc/Startup.cs
+++ Blog.Mvc/Startup.cs
@@ -1,3 +1,4 @@
+using Blog.Services.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,7 @@ namespace Blog.Mvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.LoadMyServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
