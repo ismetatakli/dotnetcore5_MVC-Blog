8d2c83db1b6e1ba362d62e50b40036e7d18a54af
İsmet Ataklı <ismetatakli@gmail.com>
2022-05-17 17:36:39 +0300

    ServiceCollectionExtensions created

---
+++ Blog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Blog.Data.Abstract;
+using Blog.Data.Concrete;
+using Blog.Data.Concrete.EntityFramework.Contexts;
+using Blog.Services.Abstract;
+using Blog.Services.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Services.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollections)
+        {
+            serviceCollections.AddDbContext<MyBlogContext>();
+            serviceCollections.AddScoped<IUnitOfWork, UnitOfWork>();
+            serviceCollections.AddScoped<ICategoryService, CategoryManager>();
+            serviceCollections.AddScoped<IArticleService, ArticleManager>();
+            return serviceCollections;
+        }
+    }
+}
