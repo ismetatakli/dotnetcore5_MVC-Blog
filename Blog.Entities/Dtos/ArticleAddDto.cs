917db24e298e43a36c8af44098c16aa268c1c0a7
İsmet Ataklı <ismetatakli@gmail.com>
2022-05-17 15:55:21 +0300

    ArticleManager and DTOs created

---
+++ Blog.Entities/Dtos/ArticleAddDto.cs
@@ -0,0 +1,54 @@
+﻿using Blog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Entities.Dtos
+{
+    public class ArticleAddDto
+    {
+        [DisplayName("Makale Başlığı")]
+        [Required(ErrorMessage = "{0} boş olmamalıdır")]
+        [MaxLength(100,ErrorMessage = "{0} {1} karakterden büyük olmamalıdır")]
+        [MinLength(5, ErrorMessage = "{0} {1} karakterden az olmamalıdır")]
+        public string Title { get; set; }
+        [DisplayName("Makale İçeriği")]
+        [Required(ErrorMessage = "{0} boş olmamalıdır")]
+        [MinLength(20, ErrorMessage = "{0} {1} karakterden az olmamalıdır")]
+        public string Content { get; set; }
+        [DisplayName("Makale Resmi")]
+        [Required(ErrorMessage = "{0} boş olmamalıdır")]
+        [MaxLength(250, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır")]
+        [MinLength(5, ErrorMessage = "{0} {1} karakterden az olmamalıdır")]
+        public string Thumbnail { get; set; }
+        [DisplayName("Makale Tarihi")]
+        [Required(ErrorMessage = "{0} boş olmamalıdır")]
+        [DisplayFormat(ApplyFormatInEditMode = true,DataFormatString ="{0:dd/MM/yyyy}")]
+        public DateTime Date { get; set; }
+        [DisplayName("SEO Yazar")]
+        [Required(ErrorMessage = "{0} boş olmamalıdır")]
+        [MaxLength(50, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır")]
+        [MinLength(0, ErrorMessage = "{0} {1} karakterden az olmamalıdır")]
+        public string SeoAuthor { get; set; }
+        [DisplayName("SEO Açıklama")]
+        [Required(ErrorMessage = "{0} boş olmamalıdır")]
+        [MaxLength(150, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır")]
+        [MinLength(0, ErrorMessage = "{0} {1} karakterden az olmamalıdır")]
+        public string SeoDescription { get; set; }
+        [DisplayName("SEO Etiketleri")]
+        [Required(ErrorMessage = "{0} boş olmamalıdır")]
+        [MaxLength(100, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır")]
+        [MinLength(0, ErrorMessage = "{0} {1} karakterden az olmamalıdır")]
+        public string SeoTags { get; set; }
+        [DisplayName("Kategori")]
+        [Required(ErrorMessage = "{0} boş olmamalıdır")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        [DisplayName("Aktiflik Durumu")]
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Blog.Services/Concrete/ArticleManager.cs
@@ -0,0 +1,116 @@
+﻿using Blog.Data.Abstract;
+using Blog.Entities.Dtos;
+using Blog.Services.Abstract;
+using Blog.Shared.Utilities.Results.Abstract;
+using Blog.Shared.Utilities.Results.ComplexTypes;
+using Blog.Shared.Utilities.Results.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Services.Concrete
+{
+    public class ArticleManager : IArticleService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ArticleManager(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public Task<IResult> Add(ArticleAddDto articleAddDto, string createdByName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResult> Delete(int articleId, string modifiedByName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IDataResult<ArticleDto>> Get(int articleId)
+        {
+            var article = await _unitOfWork.Articles.GetAsync(a=>a.Id == articleId,a=>a.User,a=>a.Category);
+            if (article!=null)
+            {
+                return new DataResult<ArticleDto>(ResultStatus.Success, new ArticleDto {
+                    Article = article,
+                    ResultStatus = ResultStatus.Success
+                });
+            }
+            return new DataResult<ArticleDto>(ResultStatus.Error, "Makale bulunamadı", null);
+        }
+
+        public async Task<IDataResult<ArticleListDto>> GetAll()
+        {
+            var articles = await _unitOfWork.Articles.GetAllAsync(null, a => a.User, a => a.Category);
+            if (articles.Count>-1)
+            {
+                return new DataResult<ArticleListDto>(ResultStatus.Success, new ArticleListDto {
+                    Articles = articles,
+                    ResultStatus = ResultStatus.Success
+                });
+            }
+            return new DataResult<ArticleListDto>(ResultStatus.Error, "Makaleler bulunamadı", null);
+        }
+
+        public async Task<IDataResult<ArticleListDto>> GetAllByCategory(int categoryId)
+        {
+            var result = await _unitOfWork.Categories.AnyAsync(c => c.Id == categoryId);
+            if (result)
+            {
+                var articles = await _unitOfWork.Articles.GetAllAsync(a => a.CategoryId == categoryId && !a.isDeleted && a.isActive, a => a.User, a => a.Category);
+                if (articles.Count > -1)
+                {
+                    return new DataResult<ArticleListDto>(ResultStatus.Success, new ArticleListDto
+                    {
+                        Articles = articles,
+                        ResultStatus = ResultStatus.Success
+                    });
+                }
+                return new DataResult<ArticleListDto>(ResultStatus.Error, "Makaleler bulunamadı", null);
+            }
+            return new DataResult<ArticleListDto>(ResultStatus.Error, "Kategori bulunamadı", null);
+        }
+
+        public async Task<IDataResult<ArticleListDto>> GetAllByNonDeleted()
+        {
+            var articles = await _unitOfWork.Articles.GetAllAsync(a => !a.isDeleted, ar => ar.User, ar => ar.Category);
+            if (articles.Count > -1)
+            {
+                return new DataResult<ArticleListDto>(ResultStatus.Success, new ArticleListDto
+                {
+                    Articles = articles,
+                    ResultStatus = ResultStatus.Success
+                });
+            }
+            return new DataResult<ArticleListDto>(ResultStatus.Error, "Makaleler bulunamadı", null);
+        }
+
+        public async Task<IDataResult<ArticleListDto>> GetAllByNonDeletedAndActive()
+        {
+            var articles = await _unitOfWork.Articles.GetAllAsync(a => !a.isDeleted && a.isActive, a => a.User, a => a.Category);
+            if (articles.Count > -1)
+            {
+                return new DataResult<ArticleListDto>(ResultStatus.Success, new ArticleListDto
+                {
+                    Articles = articles,
+                    ResultStatus = ResultStatus.Success
+                });
+            }
+            return new DataResult<ArticleListDto>(ResultStatus.Error, "Makaleler bulunamadı", null);
+        }
+
+        public Task<IResult> HardDelete(int articleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResult> Update(ArticleUpdateDto articleUpdateDto, string modifiedByName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
