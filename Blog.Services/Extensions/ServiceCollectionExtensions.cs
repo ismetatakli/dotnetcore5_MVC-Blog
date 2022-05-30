977735b2671944bd0b66dba21cd3da786f5793f8
İsmet Ataklı <ismetatakli@gmail.com>
2022-05-30 16:07:29 +0300

    Password, Username and Email options configured

--- Blog.Services/Extensions/ServiceCollectionExtensions.cs
+++ Blog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -18,7 +18,19 @@ namespace Blog.Services.Extensions
         public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollections)
         {
             serviceCollections.AddDbContext<MyBlogContext>();
-            serviceCollections.AddIdentity<User, Role>().AddEntityFrameworkStores<MyBlogContext>();
+            serviceCollections.AddIdentity<User, Role>(options =>
+            {
+                //Password Options
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 0;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                //Name and Mail Options
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<MyBlogContext>();
             serviceCollections.AddScoped<IUnitOfWork, UnitOfWork>();
             serviceCollections.AddScoped<ICategoryService, CategoryManager>();
             serviceCollections.AddScoped<IArticleService, ArticleManager>();
