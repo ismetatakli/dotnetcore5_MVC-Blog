512f02dc6cda7f570a522e46d43e4e37c16081a3
İsmet Ataklı <ismetatakli@gmail.com>
2022-05-30 17:12:46 +0300

    userAddDto and annotations created

---
+++ Blog.Entities/Dtos/UserAddDto.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Entities.Dtos
+{
+    public class UserAddDto
+    {
+        [DisplayName("Kullanıcı Adı")]
+        [Required(ErrorMessage = "{0} alanı gereklidir.")]
+        [MaxLength(50, ErrorMessage = "{0} alanı maximum {1} karakter olmalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} alanı minimum {1} karakter olmalıdır.")]
+        public string UserName { get; set; }
+        [DisplayName("Şifre")]
+        [Required(ErrorMessage = "{0} alanı gereklidir.")]
+        [MaxLength(30, ErrorMessage = "{0} alanı maximum {1} karakter olmalıdır.")]
+        [MinLength(5, ErrorMessage = "{0} alanı minimum {1} karakter olmalıdır.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DisplayName("E-Posta Adresi")]
+        [Required(ErrorMessage = "{0} alanı gereklidir.")]
+        [MaxLength(100, ErrorMessage = "{0} alanı maximum {1} karakter olmalıdır.")]
+        [MinLength(10, ErrorMessage = "{0} alanı minimum {1} karakter olmalıdır.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [DisplayName("Telefon Numarası")]
+        [Required(ErrorMessage = "{0} alanı gereklidir.")]
+        [MaxLength(13, ErrorMessage = "{0} alanı maximum {1} karakter olmalıdır.")]
+        [MinLength(13, ErrorMessage = "{0} alanı minimum {1} karakter olmalıdır.")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+        [DisplayName("Profil Resmi")]
+        [Required(ErrorMessage = "{0} gereklidir.")]
+        [DataType(DataType.Upload)]
+        public IFormFile Picture { get; set; }
+    }
+}
