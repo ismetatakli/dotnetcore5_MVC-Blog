f2c5cd9c0eeb127e591bd911b2aeb7fdab07d037
İsmet Ataklı <ismetatakli@gmail.com>
2022-06-09 11:13:32 +0300

    UserLoginDto created

---
+++ Blog.Entities/Dtos/UserLoginDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Entities.Dtos
+{
+    public class UserLoginDto
+    {
+        [DisplayName("E-Posta Adresi")]
+        [Required(ErrorMessage = "{0} alanı gereklidir.")]
+        [MaxLength(100, ErrorMessage = "{0} alanı maximum {1} karakter olmalıdır.")]
+        [MinLength(10, ErrorMessage = "{0} alanı minimum {1} karakter olmalıdır.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [DisplayName("Şifre")]
+        [Required(ErrorMessage = "{0} alanı gereklidir.")]
+        [MaxLength(30, ErrorMessage = "{0} alanı maximum {1} karakter olmalıdır.")]
+        [MinLength(5, ErrorMessage = "{0} alanı minimum {1} karakter olmalıdır.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DisplayName("Beni Hatırla")]
+        public bool RememberMe { get; set; }
+    }
+}
